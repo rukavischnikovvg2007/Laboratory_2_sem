711033b9fa28f23848eb2503b67772ec317cc442
Владимир <rukavischnikov2000@gmail.com>
2026-05-19 19:12:46 +0400

    Update etl_variant_10.py

--- dags/etl_variant_10.py
+++ dags/etl_variant_10.py
@@ -1,6 +1,6 @@
-import pendulum
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+import pendulum
 
 PROJECT_DIR = "/opt/airflow"
 CONFIG = f"{PROJECT_DIR}/configs/variant_10.yml"
@@ -13,31 +13,55 @@ default_args = {
 
 with DAG(
     dag_id="etl_variant_10",
-    start_date=pendulum.today('UTC').add(days=-1),
+    start_date=pendulum.datetime(2026, 5, 19, tz="UTC"),
     schedule="*/5 * * * *",
     catchup=False,
     default_args=default_args,
-    tags=["semester2", "etl", "week11"],
+    tags=["semester2", "etl", "week12"],
 ) as dag:
 
     extract = BashOperator(
         task_id="extract",
-        bash_command=f"cd {PROJECT_DIR} && python {PROJECT_DIR}/src/extract.py {CONFIG}",
+        bash_command=f"""
+        python {PROJECT_DIR}/src/extract.py \
+        --config {CONFIG} \
+        --run_date {{{{ ds }}}} \
+        --start "{{{{ data_interval_start }}}}" \
+        --end "{{{{ data_interval_end }}}}"
+        """,
     )
 
     transform = BashOperator(
         task_id="transform",
-        bash_command=f"cd {PROJECT_DIR} && python {PROJECT_DIR}/src/transform.py {CONFIG}",
+        bash_command=f"""
+        python {PROJECT_DIR}/src/transform.py \
+        --config {CONFIG} \
+        --run_date {{{{ ds }}}} \
+        --start "{{{{ data_interval_start }}}}" \
+        --end "{{{{ data_interval_end }}}}"
+        """,
+    )
+
+    dq = BashOperator(
+        task_id="dq",
+        bash_command=f"""
+        python {PROJECT_DIR}/src/dq.py \
+        --config {CONFIG} \
+        --run_date {{{{ ds }}}} \
+        --start "{{{{ data_interval_start }}}}" \
+        --end "{{{{ data_interval_end }}}}"
+        """,
     )
 
     load = BashOperator(
         task_id="load",
-        bash_command=f"cd {PROJECT_DIR} && python {PROJECT_DIR}/src/load_to_postgres.py {CONFIG}",
-)
-
-    dq = BashOperator(
-        task_id="dq",
-        bash_command=f"cd {PROJECT_DIR} && python {PROJECT_DIR}/src/dq.py {CONFIG}",
+        bash_command=f"""
+        python {PROJECT_DIR}/src/load_to_postgres.py \
+        --config {CONFIG} \
+        --run_date {{{{ ds }}}} \
+        --start "{{{{ data_interval_start }}}}" \
+        --end "{{{{ data_interval_end }}}}"
+        """,
     )
 
-    extract >> transform >> load >> dq
+    extract >> transform >> dq >> load
